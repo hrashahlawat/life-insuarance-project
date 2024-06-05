d6c267cfa3336feb74211aac9f534ad6d2e97dc5
hrashahlawat <164475949+hrashahlawat@users.noreply.github.com>
2024-06-05 15:13:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ function mydata()
+ {
+ 
+ var a = document.getElementById("first1").value;
+ var b = document.getElementsByClassName("first").value;
+
+if(a==""||b=="")
+{
+    alert("all fields are mendatory");
+}
+ };
